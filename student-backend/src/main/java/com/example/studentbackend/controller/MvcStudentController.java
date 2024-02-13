f39097537674a505e65e019b66aa105d42c1dcc4
abdulkerim <abdu0921@hotmail.com>
2024-02-12 22:48:06 -0500

    modified

---
+++ student-backend/src/main/java/com/example/studentbackend/controller/MvcStudentController.java
@@ -0,0 +1,30 @@
+package com.example.studentbackend.controller;
+
+import com.example.studentbackend.dto.StudentDTO;
+import com.example.studentbackend.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class MvcStudentController {
+    private final StudentService studentService;
+    @GetMapping
+    public String getAllStudents(Model model){
+        List<StudentDTO> studentDTOS = studentService.getAllStudents();
+        model.addAttribute("students",studentDTOS);
+        return "student/list";
+    }
+    @GetMapping("/hello")
+    public String getHello(Model model){
+        List<StudentDTO> list = studentService.getAllStudents();
+        model.addAttribute("students",list);
+        return "student/hello";
+    }
+}
